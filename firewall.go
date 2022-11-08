a2b834088f36f4ff96368b4ec84ebb6804c4f5b7
peroxidee <iluvwerewolves333@gmail.com>
2022-11-07 19:58:33 -0800

    firewall rule lister

---
+++ firewall.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	wapi "github.com/iamacarpet/go-win64api"
+	"go.uber.org/zap"
+)
+
+type FirewallList struct {
+	name        string
+	desc        string
+	accname     string
+	servname    string
+	localports  string
+	remoteports string
+	localaddr   string
+	remoteaddr  string
+}
+
+func Firewalllister() []FirewallList {
+	fireslice := make([]FirewallList, 0)
+	svc, err := wapi.FirewallRulesGet()
+	if err != nil {
+		zap.S().Error("Getting services failed!")
+	}
+	for _, v := range svc {
+
+		helium := FirewallList{
+			name:        v.Name,
+			desc:        v.Description,
+			accname:     v.ApplicationName,
+			servname:    v.ServiceName,
+			localports:  v.LocalPorts,
+			remoteports: v.RemotePorts,
+			localaddr:   v.LocalAddresses,
+			remoteaddr:  v.RemoteAddresses}
+
+		fireslice = append(fireslice, helium)
+
+	}
+	return fireslice
+
+}
--- go.mod
+++ go.mod
@@ -10,4 +10,7 @@ require (
 	github.com/google/glazier v0.0.0-20211029225403-9f766cca891d // indirect
 	github.com/iamacarpet/go-win64api v0.0.0-20220720120512-241a9064deec // indirect
 	github.com/scjalliance/comshim v0.0.0-20190308082608-cf06d2532c4e // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 )
